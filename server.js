bdddabb8dd8af9cc43e1ef31d6d32ed51475d2ba
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-09-08 17:49:19 -0500

    missing put statement, but close enough for me to call it done

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = express();
 const port = 1234;
 let users = [ {'id': 1, 'name': 'Trevor Buchanan'} ];
 
+server.use(express.json());
 server.listen(port);
 
 server.get('/', function(req, res) {
@@ -20,3 +21,25 @@ server.get('/api/users/:id', function(req, res) {
     res.json(user);
   } else res.status(404).json({message: 'not found'});
 });
+
+server.post('/api/users', function(req, res) {
+  if (req.body.name) {
+    users.push({ id: 1 + users.reduce((acc, user) => user.id > acc ? acc = user.id : acc, 0),
+    name: req.body.name})
+    console.log(users);
+    res.status(200).send();
+  } else {
+    res.status(500).send();
+  }
+});
+
+server.delete('/api/users/:id', function(req, res) {
+  const result = users.filter((user) => user.id != req.params.id);
+  if (result.length < users.length) {
+    res.status(200).send();
+    users = result;
+  } else {
+    res.status(500).json({message: `user with id ${req.params.id} does not exist`});
+  }
+  console.log(users)
+})
